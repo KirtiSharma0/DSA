a5f9d57ce3a248fb65942e862b744c8647e19fee
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-03-11 20:43:52 +0530

    initial commit

---
+++ array1/declare.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){ 
+    int arr[5]; //decalaration
+
+    //iniatialization
+    arr[0]=21;
+    arr[1]=31;
+    arr[2]=41;
+    arr[3]=51;
+    arr[4]=61;
+
+    cout<<arr[3];
+    return 0;
+}
