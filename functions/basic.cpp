f1e07d44836fd822b2bf44e55c62aabde6823906
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-03-15 16:18:53 +0530

    initial commit

---
+++ functions/basic.cpp
@@ -0,0 +1,14 @@
+//print multiple times using functions
+#include<iostream>
+using namespace std;
+
+void greeting(){
+    cout<<"Good morning ";
+    cout<<"Have a nice day"<<endl;;
+}
+
+int main(){
+    greeting(); //func. calling
+    greeting();
+    greeting();
+}
