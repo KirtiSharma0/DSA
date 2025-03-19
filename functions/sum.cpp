d854117bec207cf06008471ef2f324d275bd95fa
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-03-19 19:35:47 +0530

    initial commit

---
+++ functions/sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int sum(int x , int y){
+      return x+y;
+}
+int main(){
+    int x = 10;
+    int y = 20;
+    cout<<sum(x,y)<<endl;
+    cout<<sum(5,5);
+}
