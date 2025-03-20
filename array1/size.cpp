0cd33c16bc734c4a6f8bf3d1d77b9585be7356a0
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-03-21 00:56:11 +0530

    initial commit

---
+++ array1/size.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {1,233,456,25,203,46,26,35,87,56,896,57,457,687,52,96,41,35,82,50,63,0,5,0,5,0,5,4,45};
+     int n = sizeof(arr)/sizeof arr[0];
+     cout<<n;
+    return 0;
+}
