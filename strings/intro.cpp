af203293b03f27660556a5ce5ba4c3653701c55f
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-03-31 12:33:09 +0530

    initial commit

---
+++ strings/intro.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {1,2,3,4,5};
+
+    //character 
+   char ch = 'B';
+   
+   char str[3]  = {'a','b','c'};
+   for(int i=0;i<3;i++){
+    cout<<str[i]<<" ";
+   }
+   return 0 ;
+}
