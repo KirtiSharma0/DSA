203b81c4fcee7da47c2a8dc7da8c69de9d5854b4
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-03-31 18:49:53 +0530

    initial commit

---
+++ strings/updation.cpp
@@ -0,0 +1,14 @@
+//updation of single character of string
+
+#include<iostream>
+using namespace std;
+int main(){
+    string s = "KIRTI";
+    cout<<s<<endl;
+
+    //updating new value
+    s[0] = 'V';
+    cout<<s;
+
+    return 0;
+}
