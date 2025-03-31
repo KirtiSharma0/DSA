903a30bcafe9bbdfddc356b0984dd489f825c61c
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-03-31 18:04:04 +0530

    initial commit

---
+++ strings/characters.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char first = 'b';
+    cout<<first ;
+    cout<<endl;
+
+    char sp = '&';
+    cout<<sp;
+    cout<<endl;
+
+    //ASCII value of character
+    char ch = 'B';
+    cout<<(int)ch;
+    
+    return 0;
+}
