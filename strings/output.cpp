5fea5244e6e406b28bbcf77e50f4041382aaa2b6
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-03-31 18:01:03 +0530

    initial commit

---
+++ strings/output.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char str[5] = {'a','b','c','d','e'};
+    cout<<str[1]<<endl;
+    cout<<str[3]<<endl;
+    cout<<str[4]<<endl;
+
+    return 0;
+}
