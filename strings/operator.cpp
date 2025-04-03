2760422ce6e1339d5a9684d02744b29b2f7ca2ea
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-04-03 20:27:36 +0530

    initial commit

---
+++ strings/operator.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s = "kirti";
+    cout<<s<<endl;
+    string m = "sharma";
+    s = s+m;
+    cout<<s<<endl;
+    
+    string s2 = "white";
+    s2 = s2 + "flower";
+    cout<<s2<<endl;
+
+    string s3 = "Age is ";
+    s3 = s3 + "24";
+    cout<<s3;
+    return 0;
+}
