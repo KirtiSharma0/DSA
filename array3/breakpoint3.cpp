eee648918a2426cb6eee55b3ba50901e273898fe
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-04-07 21:24:58 +0530

    initial commit

--- array3/breakpoint3.cpp
+++ array3/breakpoint3.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
+
 int main(){
     int n;
     cin>>n; //size
@@ -8,7 +10,7 @@ int main(){
         cin>>arr[i];
     }
     //finding breakpoint
-    int point;
+    int point=-1;
     for(int i=0;i<n-1;i++){
         if(arr[i]<arr[i+1]){
         point = i;
@@ -22,9 +24,29 @@ int main(){
     for(int i=point+1;i<n;i++){
        if(arr[i]>arr[point]){
         int z = arr[i+1] - arr[point];
-        if(z<diff) close = i;
+        if(z<diff) {
+            diff = z;
+            close = i;
+        }
        }
     }
-    cout<<"Greater and closest value is present at index = "<<close;
+    cout<<"Greater and closest value is present at index = "<<close<<endl;
+
+    swap(arr[point],arr[close]);
+     
+    int i=point+1;
+    int j=n-1;
+    while(i<=j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+    return 0;
+
 }
  
