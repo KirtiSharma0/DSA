6ef65084d846a88aefbd46ad5a53c36c5e708499
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-04-08 20:06:01 +0530

    initial commit

--- bubble-sorting/first.cpp
+++ bubble-sorting/first.cpp
@@ -7,9 +7,16 @@ int main(){
     for(int i=0;i<5;i++){
         cin>>v[i];
     }
-    sort(v.begin(),v.end());
+    sort(v.begin(),v.end());  //ascending
     for(int i=0;i<5;i++){
-        cout<<v[i]<<" ";
+        cout<<v[i]<<" ";  
     }
+    cout<<endl;
+
+    reverse(v.begin(),v.end()); //descending = sort and reverse
+    for(int i=0;i<5;i++){
+        cout<<v[i]<<" ";  
+    }
+
     return 0;
 }
