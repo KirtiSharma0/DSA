89c9c554c9e7bb3f841ebf50960a20ac738d22d2
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-04-10 20:57:53 +0530

    initial commit

---
+++ strings2/sorting2.cpp
@@ -0,0 +1,17 @@
+//sorting in same prefixes 
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+//     string s[] = {"flower","flow","flight","fly","flat"};
+//     cout<<s;
+//    sort(s.begin(),s.end());
+//    cout<<s;
+
+string s;
+    cin>>s;
+    sort(s.begin(),s.end());
+    cout<<s<<endl;
+   return 0;
+}
