992d1a93413108bf04868667162eca4e290e5460
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-04-12 19:36:14 +0530

    initial commit

--- bubble-sorting/bubble_algo.cpp
+++ bubble-sorting/bubble_algo.cpp
@@ -10,7 +10,7 @@ int main(){
 
     for(int i=0;i<n-1;i++){  //number of passes
        
-        for(int j=0;j<n-1;j++){  //traversing array
+        for(int j=0;j<n-1-i;j++){  //traversing array
             if(arr[j]>arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];  //swapping
