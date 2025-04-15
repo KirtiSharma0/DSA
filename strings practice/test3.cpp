76949163f38c6ac7441230e7c816400cfeabbac4
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-04-16 00:46:06 +0530

    initial commit

---
+++ strings practice/test3.cpp
@@ -0,0 +1,16 @@
+//print true if string has even number of consonants and false otherwise
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int count = 0;
+    for(int i=0;i<s.length();i++){
+        if(s[i]!='a'|| s[i]!='e'|| s[i]!='i'|| s[i]!='o'|| s[i]!='u'|| s[i]!='A'|| s[i]!='E'|| s[i]!='I'|| s[i]!='O'|| s[i]!='U')
+        count++;
+    }
+    if(count%2==0) cout<<"True";
+    else cout<<"false";
+
+    return 0;
+}
