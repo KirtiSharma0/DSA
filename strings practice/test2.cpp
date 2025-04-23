d0122c7089275030b6c6a88fa869a207fe46d471
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-04-23 21:17:55 +0530

    initial commit

--- strings practice/test2.cpp
+++ strings practice/test2.cpp
@@ -5,8 +5,16 @@ using namespace std;
 int main(){
     string s;
     cin>>s;
-
-    reverse(s.begin(),s.end());
+    
+   int i=0;
+   int j=s.length()-1;
+   while(i<=j){
+    int temp = s[i];
+    s[i] = s[j];
+    s[j] = temp;
+    i++;
+    j--;
+   }
     cout<<s;
 
     return 0;
