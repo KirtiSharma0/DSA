7a096ba9b6d5da2ecd2eb31f31f69e810a74b099
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-04-27 20:37:02 +0530

    initial commit

--- strings practice/test3.cpp
+++ strings practice/test3.cpp
@@ -6,7 +6,7 @@ int main(){
     cin>>s;
     int count = 0;
     for(int i=0;i<s.length();i++){
-        if(s[i]!='a'|| s[i]!='e'|| s[i]!='i'|| s[i]!='o'|| s[i]!='u'|| s[i]!='A'|| s[i]!='E'|| s[i]!='I'|| s[i]!='O'|| s[i]!='U')
+        if(s[i]!='a'&& s[i]!='e'&& s[i]!='i'&& s[i]!='o'&& s[i]!='u'&& s[i]!='A'&& s[i]!='E'&& s[i]!='I'&& s[i]!='O'&& s[i]!='U')
         count++;
     }
     if(count%2==0) cout<<"True";
