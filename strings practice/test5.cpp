d75fcd8093a584505f62c8fb476e5b10ca88728e
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-05-02 21:21:57 +0530

    initial commit

---
+++ strings practice/test5.cpp
@@ -0,0 +1,18 @@
+//count number of words in sentence and store words in vector
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    getline(cin,s);
+    int count = 0;
+
+    //if 2 words means 1space
+    //n words have (n-1)space
+
+    for(int i=0;i<s.length();i++){
+        string temp = s[i];
+        while(s[i]!=' '){
+             
+        }
+    }
+}
