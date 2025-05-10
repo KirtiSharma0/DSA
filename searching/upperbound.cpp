9485fb6c60c89d4fe9209d14c328d58e24ce7b85
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-05-10 19:19:48 +0530

    initial commit

---
+++ searching/upperbound.cpp
@@ -0,0 +1,16 @@
+//find lower bound if target element is not found
+// 1 2 4 5 15 18 21 24  target=20 , notfound , lowerbound=18 , upperbound=21
+
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[9] = {1, 2, 4, 5, 15, 18, 21, 24};
+    int x = 20;
+    for(int i=0;i<9;i++){
+        if(arr[i]>x){
+           cout<<arr[i];
+           break;
+        }
+    }
+    return 0;
+}
