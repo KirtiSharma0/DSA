a412092b35f6c1d42410714df5bf96b67f966e16
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-06-11 21:42:00 +0530

    initial commit

---
+++ Hashmaps/setbasic.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+int main(){
+    unordered_set<int>s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(3);
+    s.insert(4);
+    s.insert(5);
+
+    cout<<s.size()<<endl;
+
+    //to print elements using for each loop
+    for(int ele:s){
+    cout<<ele<<" ";
+    }
+cout<<endl;
+
+    s.erase(3);
+    //printing after deletion
+    for(int ele:s){
+    cout<<ele<<" ";
+    }
+}
