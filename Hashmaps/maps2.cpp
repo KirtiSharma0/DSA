e3032209f89263c1c0a185695b062ed951a8bff8
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-06-11 22:01:00 +0530

    initial commit

---
+++ Hashmaps/maps2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+int main(){
+    unordered_map<string,int>m;
+    pair<string,int>p;
+    //maps are used in terms of keys and values
+    m["Rahul"] = 12;
+    m["Priya"] = 34;
+    m["Sachin"] = 22;
+    m["Angel"] = 56;
+    m["Shivam"] = 43;
+    //it is the format to insert value in string and their respective integer
+
+    for(pair<string,int> p:m){
+        cout<<p.first<<" "<<p.second<<endl;
+    }
+    cout<<m.size()<<endl;
+
+    m.erase("Priya");
+    m.erase("Shivam");
+    //new size
+    cout<<m.size();
+
+    return 0;
+}
