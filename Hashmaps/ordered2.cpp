e2e7dd188e32c4c149aa83e38c0c98eea17bc9f3
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-06-15 21:12:27 +0530

    iitial commit

---
+++ Hashmaps/ordered2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<unordered_map>
+#include<map>
+using namespace std;
+int main(){
+    map<string,int>m;
+    m["riya"] = 12;
+    m["sohan"] = 42;
+    m["anna"] = 64;
+    m["dart"] = 8;
+
+    for(auto x : m){
+       string name = x.first;
+       int age = x.second;
+    //    cout<<x.first<<" "<<endl;
+       cout<<x.second<<" "<<endl;
+
+    }
+}
