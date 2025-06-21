028ed853d09ce0018783f3310bd54a6ceb14e052
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-06-21 12:45:22 +0530

    initial commit

---
+++ Stacks/pop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    stack<int>st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+    st.push(6);
+    cout<<st.size()<<endl;  //to calculate size of stack
+
+    st.pop();
+    cout<<st.size()<<endl;
+    st.pop(); //deletes the topmost element
+    cout<<st.size();
+}
