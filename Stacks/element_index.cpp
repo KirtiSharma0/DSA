1bc4ecfed5be0e7f5f48376ee31f769024e265d8
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-06-22 21:50:38 +0530

    initial commit

---
+++ Stacks/element_index.cpp
@@ -0,0 +1,31 @@
+//push 100 at 2nd index of stack
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    stack<int>st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+    st.push(60);
+    //empty st in rt till index value more than give index
+    stack<int>rt;
+    int index = 2;
+    while(st.size()>index){
+        rt.push(st.top());
+        st.pop();
+    }
+    st.push(100);
+    while(rt.size()>0){
+        st.push(rt.top());
+        rt.pop();
+    }
+
+    //print updated stack
+    while(st.size()>0){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+}
