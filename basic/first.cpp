c5c0d81e1ccfddca85e57c675c8207fdcac36e8b
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-07-10 00:03:27 +0530

    initial commit

---
+++ basic/first.cpp
@@ -0,0 +1,35 @@
+ #include <iostream>
+using namespace std;
+
+ int main(){
+    cout<<"MY NAME IS KIRTI SHARMA\n";
+    int a= 123;
+    cout<<a << endl;
+
+    char b = 'V' ;
+    cout<< b << endl;
+
+    bool b1 = true;
+    cout<< b1 << endl;
+
+    bool b2 = false;
+    cout<< b2 << endl;
+
+    float f = 1.2;
+    cout<< f <<endl;
+
+    double d = 1.44;
+    cout << d << endl;
+
+    int size = sizeof(a);
+    cout<< "SIZE OF A is " << size << endl;
+ 
+    int h = 2/5;
+    cout<< h << endl;
+
+    int h1 = 2.0/5;
+    cout<< h1 << endl;
+
+
+    return 0;
+ }
