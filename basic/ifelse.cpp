d888f2574fb356d203fd1dd7a5ef9d6a57953c9b
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-07-13 23:24:50 +0530

    initial commit

---
+++ basic/ifelse.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+  int main() {
+     
+    int a,b ;
+    cin>>a>>b ; 
+
+ cout<< " value of a and b is " << a << "  " << b << endl;
+  if(a>b) {
+    cout<<"A is greater";
+  }
+ else {
+    cout<<"B is greater";
+ }
+
+    
+    return 0;
+}
