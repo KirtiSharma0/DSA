144e351b3810ef221c250b4ffc39f3eb691fb629
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-07-14 20:58:47 +0530

    initial commit

--- Stacks/outputStack.cpp
+++ Stacks/outputStack.cpp
@@ -3,15 +3,14 @@
 using namespace std;
 int main(){
     stack<int>st;
-    
-    st.push(60);
+     
     st.push(50);
     st.push(40);
     st.push(30);
     st.push(20);
     st.push(10);
     
-//printing in reverse order
+//output printing in reverse order
      while(st.size()>0){
         cout<<st.top()<<" ";
         st.pop();
