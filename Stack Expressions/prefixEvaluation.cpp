c666ee3b0e1343d3a3d02f1cc6e8952f292658ba
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-08-03 19:59:02 +0530

    initial commit

--- Stack Expressions/prefixEvaluation.cpp
+++ Stack Expressions/prefixEvaluation.cpp
@@ -6,18 +6,19 @@ using namespace std;
 
 int solve(int val1,int val2, char op){
     if(op=='+') val1+val2;
-    else if(op=='-') val1-val2;
-    else if(op=='*') val1*val2;
-    else if(op=='/') val1/val2;
+    else if(op=='-') return val1-val2;
+    else if(op=='*') return val1*val2;
+    else if(op=='/') return val1/val2;
 }
 int main(){
 string s = "-/*+79483";
 stack<int>st;
-   for(int i=0;i<s.length();i++){
+
+   for(int i=s.length()-1;i<s.length();i--){
     if(s[i]==(s[i]-48)){  //digit
       st.push(s[i]);
     }
-    if(s[i]=='+' ||s[i]=='-'|| s[i]=='*'||s[i]=='/'){  //perform calculation
+    else if(s[i]=='+' ||s[i]=='-'|| s[i]=='*'||s[i]=='/'){  //perform calculation
         char op = s[i];
         int val1 = st.top();
         st.pop();
@@ -25,8 +26,9 @@ stack<int>st;
         st.pop();
 
         int ans = solve(val1,val2,op);
+        
         st.push(ans);     //push value of ans in stack
-    }
+        }
    }
    cout<<st.top(); //answer
 }
