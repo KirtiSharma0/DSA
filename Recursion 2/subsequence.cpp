77d42a3563ee88d4f65e7fa83505a59af0c3826c
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-08-19 20:02:38 +0530

    initial commit

---
+++ Recursion 2/subsequence.cpp
@@ -0,0 +1,29 @@
+//print all the susequence of array
+#include<iostream>
+#include<vector>
+using namespace std;
+void func(int nums[], vector<int>arr, vector<int>ans, int i ){
+   
+    //edge case
+   if(i==3){ 
+    ans.push_back(arr[i]);
+    return;
+   }
+
+   //picking an element , array value push
+   arr.push_back(nums[i]);
+   func(nums, arr, ans, i+1);   //move forward
+
+   arr.pop_back();    //to skip last ele after picking it
+
+   //not picking condition
+   func(nums, arr, ans, i+1);   //just move forward by not picking any element
+
+}
+
+int main(){
+    int nums[3] = {4,5,6};
+    vector<int>arr;
+    vector<int>ans;
+    func(nums,arr,ans,0);  //initialize i = 0
+}
